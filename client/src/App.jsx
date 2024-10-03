2d3137e52d2f1f314c127135ff05f7a38d29fde3
Josh Stringer <joshstringer@live.com>
2024-10-03 11:42:19 -0500

    added signup/login buttons, slight css adjustments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,10 +7,14 @@ function App() {
     <>
       <div className='appBox'>
           <div className='header'>
-
+              <h1>Climb Tracker</h1>
+          </div>
+          <div className='mainButtons'>
+              <button className='mainButton'>Sign Up</button>
+              <button className='mainButton'>Log In</button>
           </div>
           <div className='footer'>
-
+              <h4>This is a footer</h4>
           </div>
       </div>
     </>
