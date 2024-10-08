b9f99f5e8c76bbaf2aa7ba60f80eb86fa2c52351
Josh Stringer <joshstringer@live.com>
2024-10-08 10:20:22 -0500

    created Climb model

---
+++ server/models/Climb.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const climbSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    area: {
+        type: String,
+        required: true
+    },
+    subArea: {
+        type: String
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    grade: {
+        type: String,
+        required: true
+    },
+    length: {
+        type: Number
+    },
+    numAttempts: {
+        type: Number
+    },
+    comments: {
+        type: String
+    }
+});
+
+const Climb = mongoose.model('Climb', climbSchema);
+
+module.exports = Climb;
--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
 const Workout = require('./Workout');
+const Climb = require('./Climb');
 
-module.exports = { User, Workout };
+module.exports = { User, Workout, Climb };
