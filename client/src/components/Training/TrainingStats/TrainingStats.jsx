5a8034252d90c6ff3bc910a19baa647e7db933b9
Josh Stringer <joshstringer@live.com>
2024-10-10 14:58:39 -0500

    training stats now links to user workout data and shows number of workouts logged as a starting point

--- client/src/components/Training/TrainingStats/TrainingStats.jsx
+++ client/src/components/Training/TrainingStats/TrainingStats.jsx
@@ -1,12 +1,27 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../../utils/AuthContext';
+import { useQuery } from '@apollo/client';
+import { GET_WORKOUTS } from '../../../utils/queries';
 import './TrainingStats.css';
 
 function TrainingStats() {
+    const { user } = useContext(AuthContext);
+    const { loading, error, data } = useQuery(GET_WORKOUTS, {
+        variables: { userId: user._id },
+        skip: !user._id,
+    });
 
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error loading workouts</div>;
+
+    const workouts = data.getWorkouts || [];
+    const workoutCount = workouts.length;
     
     return (
         <>
             <div className='trainingStatsBody'>
                 TRAINING STATS<br /><br />
+                <div># of Workouts: {workoutCount}</div>
             </div>
         </>
     );
