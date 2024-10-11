7a3c08f6c557e7ae241e2be5cffb43e9d8b0a3e0
Josh Stringer <joshstringer@live.com>
2024-10-11 09:50:24 -0500

    climb stats now shows what grades youve climbed and how many of each grade

--- client/src/components/Climbs/ClimbStats/ClimbStats.jsx
+++ client/src/components/Climbs/ClimbStats/ClimbStats.jsx
@@ -1,12 +1,50 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../../utils/AuthContext';
+import { useQuery } from '@apollo/client';
+import { GET_CLIMBS } from '../../../utils/queries';
 import './ClimbStats.css';
 
 function ClimbStats() {
+    const { user } = useContext(AuthContext);
+    const { loading, error, data } = useQuery(GET_CLIMBS, {
+        variables: { userId: user._id },
+        skip: !user._id,
+    });
 
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error loading workouts</div>;
+
+    const climbs = data.getClimbs || [];
+    const climbCount = climbs.length;
+
+    const climbData = {};
+
+    climbs.forEach(climb => {
+        const grade = climb.grade;
+
+        if (!climbData[grade]) {
+            climbData[grade] = { count: 0 };
+        }
+
+        climbData[grade].count +=1;
+
+    });
     
     return (
         <>
             <div className='climbStatsBody'>
                 CLIMBING STATS<br /><br />
+                <div># of Climbs:<hr />{climbCount}</div><br />
+                <div>
+                    Grades:<hr /><br />
+                    <ul>
+                        {Object.entries(climbData).map(([grade, { count }]) => (
+                            <li key={grade}>
+                                {grade}: {count} times
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </>
     );
